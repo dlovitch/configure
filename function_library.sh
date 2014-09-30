f04b8fc54df8de757947729b2b2f6e11305fbe1f
David Lovitch <dlovitch@gmail.com>
2014-09-30 02:03:54 -0700

    Add function_library.sh

---
+++ function_library.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# get_: echo requested data
+# set_: change/add data, previous data/comments/whitespace not guaranteed 
+# update_: change/add data, previous data/comments/whitespace should not be affected
+
+# Get a sane screen width
+[ -z "${COLUMNS:-}" ] && COLUMNS=80
+
+RES_COL=60
+MOVE_TO_COL="echo -en \\033[${RES_COL}G"
+SETCOLOR_SUCCESS="echo -en \\033[1;32m"
+SETCOLOR_FAILURE="echo -en \\033[1;31m"
+SETCOLOR_WARNING="echo -en \\033[1;33m"
+SETCOLOR_NORMAL="echo -en \\033[0;39m"
+
+
+. general_functions.sh
+. network_functions.sh
